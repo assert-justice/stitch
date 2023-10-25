02b4f8897bfb622d1e4c6073b4d131799d9bbbd0
Riley <waskerdu@gmail.com>
2023-10-25 10:49:56 -0700

    cleanup

--- dist/main.js
+++
@@ -1,9 +0,0 @@
-import { Engine, Window, Graphics } from "cleo";
-import { Game } from "./game";
-Window.setStats('stitch', 1920, 1080);
-Engine.init = () => {
-    Graphics.setClearColor(1, 1, 1, 0);
-    const game = new Game();
-    Engine.update = (dt) => game.update(dt);
-    Engine.draw = () => game.draw();
-};
